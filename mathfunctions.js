02e1de4ddb0e81810dfda2c8ffb4b114bb8ef0f7
Duclier <34814457+Duclier@users.noreply.github.com>
2019-06-09 17:19:33 -0300

    Update mathfunctions.js

--- mathfunctions.js
+++ mathfunctions.js
@@ -1,3 +1,51 @@
+function inverseTRS(T,R,S){
+	
+	var inverse_T = [[0,0,0],
+                      [0,0,0,],
+                      [0,0,0]];
+
+    var inverse_R = [[0,0,0],
+                      [0,0,0,],
+                      [0,0,0]];
+
+    var inverse_S = [[0,0,0],
+                      [0,0,0,],
+                      [0,0,0]];
+
+    for (var i = 0; i < T.length; i++) {
+        for (var j = 0; j < T.length; j++) {
+            inverse_T[i][j] = T[i][j];
+            inverse_R[i][j] = R[i][j];
+            inverse_S[i][j] = S[i][j];
+
+        };
+    };
+
+
+	inverse_T[0][2]= -(inverse_T[0][2]);
+	inverse_T[1][2] = -(inverse_T[1][2]);
+	
+    try {
+		inverse_S[0][0] = (1 / (inverse_S[0][0]));
+		inverse_S[1][1] = (1 / (inverse_S[1][1]));
+	}catch(error){
+		alert(error);
+	}
+	
+
+	inverse_R[0][1] = -(inverse_R[0][1]);
+	inverse_R[1][0] = -(inverse_R[1][0]);
+
+	return mult(mult(inverse_S,inverse_R),inverse_T);
+}
+
+function distance(radius, point){
+    if(point[0]*point[0] + point[1]*point[1] > radius*radius){
+        return false;
+    }
+	return true;
+}
+
 function translate(x, y) { // dado dois pontos, x e y constroi a matriz homogenea de translação 
     return [
         [1, 0, x],
@@ -17,7 +65,7 @@ function scale(x, y) {
 
 //TODO: dado um angulo theta em graus constroi a matriz homogenea de rotação 3x3
 function rotate(theta) {
-    theta = Math.PI * theta / 180.; //transforma theta em ratianos
+    theta = Math.PI * theta / 180.; //transforma theta em radianos
     return [
         [Math.cos(theta), -Math.sin(theta), 0],
         [Math.sin(theta), Math.cos(theta), 0],
@@ -35,8 +83,16 @@ function identity(v = 1) { // identidade
 
 function transformCanvas(Width, Height) {
     return [
-        [1, 0, Width / 2.],
-        [0, -1, Height / 2.],
+        [1, 0, Width / 2],
+        [0, -1, Height / 2],
+        [0, 0, 1]
+    ];
+}
+//coordenadas do canvas para o sistema usual
+function transformUsual(Width,Height){
+	return [
+        [1, 0, -(Width / 2)],
+        [0, -1, Height / 2],
         [0, 0, 1]
     ];
 }
